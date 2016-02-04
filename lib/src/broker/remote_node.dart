981e003f7d4361f824574fdfcb1d0cea6fa45126
Kenneth Endfinger <kaendfinger@gmail.com>
2016-02-03 22:56:46 -0500

    Revert "Fix clearDanglingNodes"
    This reverts commit e763690765afcd213894ad0162cd177a0dbd12cd.

--- lib/src/broker/remote_node.dart
+++ lib/src/broker/remote_node.dart
@@ -167,7 +167,7 @@ class RemoteLinkManager implements NodeProvider, RemoteNodeCache {
     List<String> toRemove = [];
     for (String key in nodes.keys) {
       RemoteNode node = nodes[key];
-      if (node.referenceCount == 0) {
+      if (handler(node)) {
         toRemove.add(key);
       }
     }
